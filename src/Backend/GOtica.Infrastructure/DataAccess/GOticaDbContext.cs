6bcdbd5c289788c5b58003ea3279e7db8e34f8ad
Rafael <rbezerra874@gmail.com>
2026-07-31 09:29:41 +0100

    feat: configuring GOticaDbContext

---
+++ src/Backend/GOtica.Infrastructure/DataAccess/GOticaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using GOtica.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GOtica.Infrastructure.DataAccess;
+
+internal class GOticaDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<OpticalStore> OpticalStores { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOpticalStore> UserOpticalStores { get; set; }
+}
---
+++ src/Backend/GOtica.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using GOtica.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GOtica.Infrastructure;
+
+public static class DependencyInjectionExtension
+{
+    extension(IServiceCollection services)
+    {
+        public void AddInfrastructure(IConfiguration configuration)
+        {
+            AddGOticaDbContext(services, configuration);
+        }
+    }
+
+    private static void AddGOticaDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<GOticaDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+    }
+}
