5cc91e536b1f8373485e5e890590ce4e1c9ce99a
Rafael <rbezerra874@gmail.com>
2026-08-01 16:30:35 +0100

    feat: add request and response models for user registration flow

---
+++ src/Shared/GOtica.Communication/Response/ResponseRegisterUser.cs
@@ -0,0 +1,8 @@
+﻿namespace GOtica.Communication.Response;
+
+public class ResponseRegisterUser
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ResponseTokens Tokens { get; set; } = default!;
+}
---
+++ src/Shared/GOtica.Communication/Response/ResponseTokens.cs
@@ -0,0 +1,7 @@
+﻿namespace GOtica.Communication.Response;
+
+public class ResponseTokens
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+}
