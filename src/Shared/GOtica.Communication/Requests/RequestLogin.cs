838eebc7886950e41327693250be438d5612944b
Rafael Silva <8334705@formacao.iefp.pt>
2026-08-03 11:40:41 +0100

    add request login model

---
+++ src/Shared/GOtica.Communication/Requests/RequestLogin.cs
@@ -0,0 +1,7 @@
+﻿namespace GOtica.Communication.Requests;
+
+public record RequestLogin
+{
+    public string Email { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
