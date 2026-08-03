0140ba46aebcba92b5529472a0e4ad2b1c0c8a9c
Rafael Silva <8334705@formacao.iefp.pt>
2026-08-03 11:45:02 +0100

    rename response for authentications flow

--- src/Shared/GOtica.Communication/Response/ResponseRegisterUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GOtica.Communication.Response;
-
-public record ResponseRegisterUser
-{
-    public long Id { get; init; }
-    public string Name { get; init; } = string.Empty;
-    public ResponseTokens Tokens { get; init; } = default!;
-}
