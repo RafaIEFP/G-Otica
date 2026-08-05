2e617d693a23deba1b80b09f1c26caefd5124c60
Rafael Silva <8334705@formacao.iefp.pt>
2026-08-05 14:20:28 +0100

    configure authentication middleware and authorization policies

--- src/Backend/GOtica.API/Program.cs
+++ src/Backend/GOtica.API/Program.cs
@@ -1,24 +1,41 @@
 using GOtica.API.Filters;
+using GOtica.API.Handlers;
+using GOtica.API.Handlers.Requirements;
 using GOtica.API.OpenApi;
 using GOtica.Application;
 using GOtica.Infrastructure;
 using GOtica.Infrastructure.Extensions;
 using GOtica.Infrastructure.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(op => op.Filters.Add<ExceptionFilter>());
+
 builder.Services.AddOpenApi(options =>
 {
     options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
 });
 
-
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
 builder.Services.AddRouting(config => config.LowercaseUrls = true);
+
+builder.Services.AddScoped<IAuthorizationHandler, AuthenticatedUserHandler>();
+
+builder.Services.AddAuthentication(op =>
+{
+    op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearerValidated(builder.Configuration);
+
+builder.Services.AddAuthorization(op =>
+{
+    op.AddPolicy("AuthenticatedUser", policy => policy.Requirements.Add(new AuthenticatedUserRequirement()));
+});
 
 var app = builder.Build();
 
