846e6af94a3bb04d9fb055abd3fb32b59a75ce25
Rafael Silva <8334705@formacao.iefp.pt>
2026-08-05 14:15:25 +0100

    add and configure authorization handler

---
+++ src/Backend/GOtica.API/Handlers/AuthenticatedUserHandler.cs
@@ -0,0 +1,52 @@
+﻿using GOtica.API.Handlers.Requirements;
+using GOtica.Domain.Repositories.Refresh;
+using GOtica.Domain.Repositories.User;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace GOtica.API.Handlers;
+
+public class AuthenticatedUserHandler : AuthorizationHandler<AuthenticatedUserRequirement>
+{
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    private readonly IRefreshTokenReadOnlyRepository _refreshTokenReadOnlyRepository;
+
+    public AuthenticatedUserHandler(
+        IUserReadOnlyRepository userReadOnlyRepository,
+        IRefreshTokenReadOnlyRepository refreshTokenReadOnlyRepository)
+    {
+        _userReadOnlyRepository = userReadOnlyRepository;
+        _refreshTokenReadOnlyRepository = refreshTokenReadOnlyRepository;
+    }
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, AuthenticatedUserRequirement requirement)
+    {
+        var userId = context.User.FindFirstValue(JwtRegisteredClaimNames.NameId);
+        var accessTokenIdentifier = context.User.FindFirstValue(JwtRegisteredClaimNames.Jti);
+
+        if (string.IsNullOrWhiteSpace(userId) || string.IsNullOrWhiteSpace(accessTokenIdentifier))
+        {
+            context.Fail();
+            return;
+        }
+
+        var user = await _userReadOnlyRepository.GetUserById(Guid.Parse(userId));
+
+        if (user is null)
+        {
+            context.Fail();
+            return;
+        }
+
+        var hasRefreshToken = await 
+            _refreshTokenReadOnlyRepository.HasRefresTokenAssociated(user.Id, Guid.Parse(accessTokenIdentifier));
+
+        if (!hasRefreshToken)
+        {
+            context.Fail();
+            return;
+        }
+
+        context.Succeed(requirement);
+    }
+}
---
+++ src/Backend/GOtica.API/Handlers/JwtBearerExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace GOtica.API.Handlers;
+
+public static class JwtBearerExtensions
+{
+    public static AuthenticationBuilder AddJwtBearerValidated(this AuthenticationBuilder authenticationBuilder, IConfiguration configuration)
+    {
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey")!;
+        var issuer = configuration.GetValue<string>("Settings:Jwt:Issuer")!;
+        var audience = configuration.GetValue<string>("Settings:Jwt:Audience")!;
+
+        authenticationBuilder.AddJwtBearer(op =>
+        {
+            op.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidAudience = audience,
+
+                ValidateIssuer = true,
+                ValidIssuer = issuer,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!)),
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+        return authenticationBuilder;
+    }
+}
