744a79fbb32211b656a5c624926f9f68e0f1ab30
Rafael <rbezerra874@gmail.com>
2026-08-21 19:39:44 +0100

    add migration for invite

---
+++ src/Backend/GOtica.Infrastructure/Migrations/Versions/Version00003.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using GOtica.Domain.Enums;
+using GOtica.Infrastructure.Migrations.Services;
+
+namespace GOtica.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_INVITE, "Create table invite")]
+public class Version00003 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Invites")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("GuestEmail").AsString(255).NotNullable()
+            .WithColumn("Role").AsString(50).NotNullable()
+            .WithColumn("TokenHash").AsString(2000).NotNullable()
+            .WithColumn("Status").AsInt32().NotNullable().WithDefaultValue((int)InviteStatus.Pending)
+            .WithColumn("CreatedAt").AsUtcDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime)
+            .WithColumn("ExpiresAt").AsUtcDateTime().NotNullable()
+            .WithColumn("OpticalStoreId").AsGuid().NotNullable().ForeignKey("FK_Invites_OpticalStores_OpticalStoreId", "OpticalStores", "Id")
+            .WithColumn("InvitedByUserId").AsGuid().NotNullable().ForeignKey("FK_Invites_Users_InvitedByUserId", "Users", "Id");
+    }
+}
