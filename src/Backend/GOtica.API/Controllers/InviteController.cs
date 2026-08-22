3353549d2e6cbc605a38ba065614e14539355699
Rafael <rbezerra874@gmail.com>
2026-08-22 01:22:43 +0100

    add invite controller

---
+++ src/Backend/GOtica.API/Controllers/InviteController.cs
@@ -0,0 +1,23 @@
+﻿using GOtica.Application.UseCases.UserOpticalStore.Invite.Validade;
+using GOtica.Communication.Response;
+using GOtica.Communication.Response.UserOpticalStore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GOtica.API.Controllers;
+
+[Route("api/invites")]
+[ApiController]
+public class InviteController : ControllerBase
+{
+    [HttpGet("validate")]
+    [ProducesResponseType(typeof(ResponseValidateInvite), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseError), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Validate(
+        [FromQuery] string token,
+        [FromServices] IValidateInviteUseCase useCase)
+    {
+        var response = await useCase.Execute(token);
+
+        return Ok(response);
+    }
+}
