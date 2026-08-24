568a16ba7af456481891a5f2da2dc7c12a77ac8f
Rafael <rbezerra874@gmail.com>
2026-08-24 19:57:52 +0100

    add and configure client entity

---
+++ src/Backend/GOtica.Domain/Entities/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace GOtica.Domain.Entities;
+
+public class Client
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public string Name { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
+    public bool IsActive { get; set; } = true;
+
+    public Guid OpticalStoreId { get; set; }
+    public OpticalStore OpticalStore { get; set; } = default!;
+}
---
+++ src/Backend/GOtica.Infrastructure/Migrations/Versions/Version00004.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using GOtica.Infrastructure.Migrations.Services;
+
+namespace GOtica.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_CLIENT, "Create table client")]
+public class Version00004 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Clients")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Name").AsString(255).NotNullable()
+            .WithColumn("PhoneNumber").AsString(20).NotNullable()
+            .WithColumn("Email").AsString(255).Nullable()
+            .WithColumn("DateOfBirth").AsDate().Nullable()
+            .WithColumn("IsActive").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("OpticalStoreId").AsGuid().NotNullable()
+                .ForeignKey(
+                    "FK_Clients_OpticalStores_OpticalStoreId",
+                    "OpticalStores",
+                    "Id");
+    }
+}
