d2d77cbdf5294b756fd8a913f0f2b0c49232050d
Rafael <rbezerra874@gmail.com>
2026-08-25 17:28:56 +0100

    add request and response for register client workflow

---
+++ src/Shared/GOtica.Communication/Requests/Client/RequestRegisterClient.cs
@@ -0,0 +1,9 @@
+﻿namespace GOtica.Communication.Requests.Client;
+
+public record RequestRegisterClient
+{
+    public string Name { get; init; } = string.Empty;
+    public string PhoneNumber { get; init; } = string.Empty;
+    public string? Email { get; init; }
+    public DateOnly? DateOfBirth { get; init; }
+}
---
+++ src/Shared/GOtica.Communication/Response/Client/ResponseRegisterClient.cs
@@ -0,0 +1,7 @@
+﻿namespace GOtica.Communication.Response.Client;
+
+public record ResponseRegisterClient
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+}
