17b977fb433725425a361ae67cd5e1a89371daeb
Rafael <rbezerra874@gmail.com>
2026-08-25 23:45:26 +0100

    configure pagination

---
+++ src/Backend/GOtica.Domain/Repositories/QueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace GOtica.Domain.Repositories;
+
+public static class QueryableExtensions
+{
+    extension<T> (IQueryable<T> query)
+    {
+        public IQueryable<T> Paged(int page, int pageSize)
+        {
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
