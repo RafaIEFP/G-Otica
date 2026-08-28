1f5a5fa29df45c855276106082ccfb530aeca310
Rafael <rbezerra874@gmail.com>
2026-08-28 11:34:01 +0100

    implement stock movement in register product use case

--- src/Backend/GOtica.Application/UseCases/Product/Register/RegisterProductUseCase.cs
+++ src/Backend/GOtica.Application/UseCases/Product/Register/RegisterProductUseCase.cs
@@ -1,8 +1,12 @@
 ﻿using GOtica.Communication.Requests;
 using GOtica.Communication.Requests.Product;
 using GOtica.Communication.Response.Product;
+using GOtica.Domain.Entities;
+using GOtica.Domain.Enums;
 using GOtica.Domain.Repositories;
 using GOtica.Domain.Repositories.Product;
+using GOtica.Domain.Repositories.StockMovement;
+using GOtica.Domain.Services;
 using GOtica.Exceptions.ExceptionsBase;
 using GOtica.Exceptions.Resources;
 using Mapster;
@@ -11,14 +15,20 @@ namespace GOtica.Application.UseCases.Product.Register;
 
 public class RegisterProductUseCase : IRegisterProductUseCase
 {
+    private readonly ILoggedUser _loggedUser;
+    private readonly IStockMovementWriteOnlyRepository _stockMovementWriteOnlyRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IProductWriteOnlyRepository _productWriteOnlyRepository;
     private readonly IProductReadOnlyRepository _productReadOnlyRepository;
     public RegisterProductUseCase(
+        ILoggedUser loggedUser,
+        IStockMovementWriteOnlyRepository stockMovementWriteOnlyRepository,
         IUnitOfWork unitOfWork,
         IProductWriteOnlyRepository productWriteOnlyRepository,
         IProductReadOnlyRepository productReadOnlyRepository)
     {
+        _loggedUser = loggedUser;
+        _stockMovementWriteOnlyRepository = stockMovementWriteOnlyRepository;
         _unitOfWork = unitOfWork;
         _productWriteOnlyRepository = productWriteOnlyRepository;
         _productReadOnlyRepository = productReadOnlyRepository;
@@ -27,6 +37,8 @@ public class RegisterProductUseCase : IRegisterProductUseCase
     public async Task<ResponseRegisterProduct> Execute(Guid opticalStoreId, RequestRegisterProduct request)
     {
         request = request.Normalize();
+
+        var loggedUser = await _loggedUser.Get();
 
         Validate(request);
 
@@ -39,9 +51,23 @@ public class RegisterProductUseCase : IRegisterProductUseCase
 
         product.OpticalStoreId = opticalStoreId;
 
-        await _productWriteOnlyRepository.Add(product);
+        await _unitOfWork.ExecuteInTransaction(async () =>
+        {
+            await _productWriteOnlyRepository.Add(product);
 
-        await _unitOfWork.Commit();
+            if (product.StockQuantity > 0)
+            {
+                var stockMovement = new StockMovement
+                {
+                    QuantityChange = product.StockQuantity,
+                    Type = StockMovementType.InitialStock,
+                    ProductId = product.Id,
+                    UserId = loggedUser.Id
+                };
+
+                await _stockMovementWriteOnlyRepository.Add(stockMovement);
+            }
+        });
 
         return product.Adapt<ResponseRegisterProduct>();
     }
